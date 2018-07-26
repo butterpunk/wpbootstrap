f9d549e5a670f67e74d1fe61bcc945a6e6db999c
butterpunk <blakebutterworth@gmail.com>
2018-07-26 09:43:53 -0500

    changed bootstrap

--- front-page.php
+++ front-page.php
@@ -2,4 +2,20 @@
  
   <h1>HELLO WORLD!</h1>
 
+  <?php
+ 
+ if ( have_posts() ) {
+  
+     while ( have_posts() ) {
+  
+        echo '<h1>'.the_post().'</h1>';
+  
+         // Post data goes here.
+  
+     }
+  
+ }
+  
+ ?>
+
 <?php get_footer(); ?>
